075b9e52d17f0b5644361c7437a1ffb801723585
wellitsnoor <wellitsnoor@gmail.com>
2026-07-06 12:05:12 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import { EncryptedText } from "@/components/ui/encrypted-text"
+
+export default function Home() {
+  return (
+    <div className="flex h-fit flex-col items-center justify-center mx-auto my-auto m-auto">
+    <div className="h-fit flex flex-col items-center justify-center mb-20">
+      <Image src={"/jaswant.jpg"} alt="jaswant singh khalra" height={100} width={100} className="mb-5" />
+      <EncryptedText text={"I challenge the darkness"} className="md:text-4xl text-3xl text-center font-bold"  />
+     
+      <blockquote className="text-right ml-auto">- Jaswant Singh Khalra</blockquote>
+    </div>
+    <div className="text-left flex flex-col w-[70vw] mx-auto gap-5">
+      <p className="text-3xl font-bold">Torrenting Guide</p>
+      <ul className="flex flex-col gap-5">
+        <li>
+          <p><b>Step 1:</b> Install a torrent client like qBittorrent (it is free and open source) </p>
+      <a href="https://www.qbittorrent.org/">https://www.qbittorrent.org/</a> 
+      <p>or FDM (Free Download Manager) </p>
+      <a href="https://www.freedownloadmanager.org/">https://www.freedownloadmanager.org/</a>
+        </li>
+        <li>
+<p><b>Step 2:</b> Copy the desired magnet link:-
+      </p>
+
+      <p>1080p: <a href="magnet:?xt=urn:btih:817b30b5d3eab15bc699ba98d2e31c84f2780801&dn=Torrentio%0A1080p">2.4gb</a> | <a href="magnet:?xt=urn:btih:f258844a462f6e7ec6716976c21c5a9455c306d8&dn=Torrentio%0A1080p">1.7gb</a>
+      </p>
+        </li>
+        <li>
+<p><b>Step 3:</b> Add/Paste the magnet links to the torrent client. And the download will start.</p>
+        </li>
+      </ul>
+      
+    
+
+      
+    </div>
+    </div>
+  );
+}
